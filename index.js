cf9de20d13e2d2ce48785042b1c7c72f26417a85
unknown <ferdispam@yahoo.com>
2025-03-23 18:44:23 +0100

    Commit index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 const express = require('express');
-// const sequelize = require('./sequelize'); // Connexion à la base de données
-const userRoutes = require('./routes/users');
-const questionRoutes = require('./routes/questions');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const cors = require('cors');
+
+// CORS doit être placé en premier
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
+
+app.use(express.json());
+
+const userRoutes = require('./routes/users');
+const questionRoutes = require('./routes/questions');
 
 const { Sequelize } = require('sequelize');
 
@@ -15,26 +24,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
 });
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Autorise uniquement ce domaine. Rajouter l'url en ligne une fois que le frontent aura été mis en ligne. faire entre chrochets les deux solutions. 
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-}));
-
-app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/questions', questionRoutes);
 
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
 
 // Test de connexion à la base de données
 (async () => {
     try {
         await sequelize.authenticate();
         console.log('Connexion à la base de données réussie.');
-
-        // Synchronisation des modèles sans écraser les données existantes
         await sequelize.sync({ force: false });
         console.log('Base de données synchronisée.');
     } catch (error) {
