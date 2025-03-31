64852d109795cad1bdde975dade6119f175392b8
unknown <ferdispam@yahoo.com>
2025-03-31 20:54:34 +0200

    Commit

--- routes/users.js
+++ routes/users.js
@@ -36,27 +36,6 @@ router.post('/', async (req, res) => {
 });
 
 
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Vérifier si l'utilisateur existe
-        const user = await User.findOne({ where: { email } });
-        if (!user) return res.status(404).json({ error: 'Utilisateur non trouvé.' });
-
-        // Vérifier le mot de passe
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) return res.status(401).json({ error: 'Mot de passe incorrect.' });
-
-        // Supprimer la génération du token et renvoyer seulement l'utilisateur
-        res.json({ message: 'Connexion réussie !', user });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Impossible de se connecter.' });
-    }
-});
-
-
 // Créer un utilisateur
 router.post('/login', async (req, res) => {
     try {
